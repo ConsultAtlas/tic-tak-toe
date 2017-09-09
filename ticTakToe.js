79945bceeb29a48fac43c1992bcaba3b0e7df339
Zachary Aten <z_man@Zacharys-MacBook-Pro.local>
2017-09-09 10:52:59 -0400

    added README.md file

--- ticTakToe.js
+++ ticTakToe.js
@@ -1,14 +1,8 @@
-/*
+//**********************************
+//*          Tic-Tac-Toe           *
+//**********************************
 
-1. how to win - function
-2. what the board will look like - array
-3. turns - boolean
-4. who plays first - loop
-5. 2 players - loop
-6. 1 play per tile - loop
-7. making a turn - funtion
 
-*/
 var row1 = ["", "", ""];
 var row2 = ["", "", ""];
 var row3 = ["", "", ""];
@@ -22,4 +16,3 @@ var board = [
 
 row1[0] = "X";
 
-console.log(board);
