01190808011ef542618a35a0620c27462a6ea655
Zachary Aten <z_man@Zacharys-MacBook-Pro.local>
2017-09-09 10:48:52 -0400

    added board and row variables

---
+++ ticTakToe.js
@@ -0,0 +1,25 @@
+/*
+
+1. how to win - function
+2. what the board will look like - array
+3. turns - boolean
+4. who plays first - loop
+5. 2 players - loop
+6. 1 play per tile - loop
+7. making a turn - funtion
+
+*/
+var row1 = ["", "", ""];
+var row2 = ["", "", ""];
+var row3 = ["", "", ""];
+
+
+var board = [
+	row1,
+	row2,
+	row3,
+];
+
+row1[0] = "X";
+
+console.log(board);
